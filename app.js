6cd3b4dd999f40b1b5f8e3f261fab96f92baf234
Andrew Vandermeer <Andrew@Andrews-MacBook-Air-2.local>
2016-05-31 16:30:29 -0600

    adding add page and starting js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-  console.log("sanity check");
+  var $title = $("#title").val();
+  var $genre = $("#genre").val();
+  var $descrip = $("#description").val();
+  var $url = $("#url").val();
+  var $rating = $("#rating").val();
+  var $form = $(".addMovie")
+  $("#submit").click(function(event){
+    event.preventDefault();
+
+      $form.find($title),
+      $form.find($genre),
+      $form.find($descrip),
+      $form.find($url),
+      $form.find($rating)
+      console.log(page);
+  });
+
+  
 });
